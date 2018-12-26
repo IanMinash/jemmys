fc71ba3c3d7258b9aee6f9cf1b86ebf1d660d1d4
Ian Minara <minaraian@gmail.com>
2018-12-26 16:50:13 +0300

    Added product models

--- jemmys/main/models.py
+++ jemmys/main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """Model definition for a Product."""
+
+    name = models.CharField(max_length=65)
+    description = models.CharField(max_length=800)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=30)
+
+    class Meta:
+        """Meta definition for Product."""
+
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        """Unicode representation of Product."""
+        return f"{self.name} - {self.price}"
+
+
+class ProductPhoto(models.Model):
+    """Model definition for a Product's Photos."""
+
+    photo = models.ImageField(upload_to='product_photos')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        """Meta definition for ProductPhotos."""
+
+        verbose_name = 'Product Photo'
+        verbose_name_plural = 'Product Photos'
+
+    def __str__(self):
+        """Unicode representation of ProductPhotos."""
+        return f"{self.product.name}'s photo"
--- jemmys/settings.py
+++ jemmys/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'jemmys','static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'jemmys','media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jemmys.main'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'jemmys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATE_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+#Media
+
+MEDIA_URL = '/media/'
